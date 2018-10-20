7c0c449d81e02cb25b116cf8bb5406ef6350fa37
Marat Monnié <mtulepbayevmonnie@hawk.iit.edu>
2018-10-20 12:11:49 -0500

    Tests for the App added.

---
+++ bookapp/src/components/__tests__/App.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import App from '../../App';
+
+describe('App', () => {
+    it('should render a <div />', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('div').length).toEqual(2);
+    });
+    it('should render a <NavBar />', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('NavBar').length).toEqual(1);
+    });
+    it('should render a <Switch />', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('Switch').length).toEqual(1);
+    });
+    it('should render a <Route />', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('Route').length).toEqual(2);
+    });
+    it('should render correctly', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
