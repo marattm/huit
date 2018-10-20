871b1062f7eb0f9fe1e845ba9b6bb528aa678835
Marat Monnié <mtulepbayevmonnie@hawk.iit.edu>
2018-10-20 12:15:05 -0500

    Static rendering tests added for the SearchForm.

---
+++ bookapp/src/components/search/__tests__/SearchForm.test.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import SearchForm from '../SearchForm';
+
+
+const open = true;
+const type = 'relevance';
+const printType = 'all';
+const filter = 'all';
+const language = 'all';
+const maxResults = "10";
+
+
+
+describe('SearchForm renders properly', () => {
+    
+    it('should render div component properly', () => {
+        let wrapper = shallow(<SearchForm />)
+        let element = wrapper.find('div');
+        expect(element.length).toBe(1);
+    })
+    it('should render Form component properly', () => {
+        let wrapper = shallow(<SearchForm />)
+        let element = wrapper.find('Form');
+        expect(element.length).toBe(1);
+    })
+    it('should render FormGroup component properly', () => {
+        let wrapper = shallow(<SearchForm />)
+        let element = wrapper.find('FormGroup');
+        expect(element.length).toBe(1);
+    })
+    it('should render InputGroup component properly', () => {
+        let wrapper = shallow(<SearchForm />)
+        let element = wrapper.find('InputGroup');
+        expect(element.length).toBe(1);
+    })
+    it('should render Button component properly', () => {
+        let wrapper = shallow(<SearchForm />)
+        let element = wrapper.find('Button');
+        expect(element.length).toBe(2);
+        expect(element.get(1).props.children).toBe('Search');
+    })
+    it('should render FormControl component properly', () => {
+        let wrapper = shallow(<SearchForm />)
+        let element = wrapper.find('FormControl');
+        expect(element.length).toBe(1);
+    })
+    it('should render div component properly', () => {
+        let wrapper = shallow(<SearchForm />)
+        let element = wrapper.find('div');
+        expect(element.length).toBe(1);
+    })
+    it('should render FilterOptions component properly', () => {
+        let wrapper = shallow(<SearchForm />)
+        let element = wrapper.find('FilterOptions');
+        expect(element.length).toBe(1);
+    })
+    it('should have an initial open state', () => {
+        let wrapper = shallow(<SearchForm />);
+        expect(wrapper.state().open).toBe(true);
+    })
+    it('should render correctly', () => {
+        let wrapper = shallow(<SearchForm />)
+        expect(wrapper).toMatchSnapshot();
+    });
+})
