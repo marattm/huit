72b437a7e84bdea9acd045b9216511cae3960092
Marat Monnié <mtulepbayevmonnie@hawk.iit.edu>
2018-10-30 16:51:11 -0500

    DRYed functions in SearchResults.jsx

--- bookapp/src/components/search/SearchResults.jsx
+++ bookapp/src/components/search/SearchResults.jsx
@@ -12,190 +12,38 @@ class SearchResults extends Component {
         this.render();
     }
 
-    checkThumbnail(info) {
-        /**
-         * Check if the thumbnail info is available in the response json from the API call.
-         * @param {json} info - Book information in JSON.
-         * @return {string} URL to the book's thumbnail.
-         */
-        try {
-            if (info.imageLinks.smallThumbnail) {
-                return info.imageLinks.smallThumbnail;
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    }
-    checkTitle(info) {
-        /**
-         * Check if the title info is available in the response json from the API call.
-         * @param {json} info - Book information in JSON.
-         * @return {string} Book's title.
-         */
-        try {
-            if (info.title) {
-                return info.title;
-            }
-        } catch (error) {
-            console.log(error);
-
-        }
-    }
-    checkAuthors(info) {
-        /**
-         * Check if the authors info is available in the response json from the API call.
-         * @param {json} info - Book information in JSON.
-         * @return {string} Book's authors.
-         */
-        try {
-            if (info.authors) {
-                return info.authors;
-            }
-        } catch (error) {
-            console.log(error);
-
-        }
-    }
-    checkPublishers(info) {
-        /**
-         * Check if the pulishers info is available in the response json from the API call.
-         * @param {json} info - Book information in JSON.
-         * @return {string} Book's publisher.
-         */
-        try {
-            if (info.publisher) {
-                return info.publisher;
-            }
-        } catch (error) {
-            console.log(error);
-
-        }
-    }
-    checkPublishedDate(info) {
-        /**
-         * Check if the published date info is available in the response json from the API call.
-         * @param {json} info - Book information in JSON.
-         * @return {string} Book's publish date.
-         */
-        try {
-            if (info.publishedDate) {
-                return info.publishedDate;
-            }
-        } catch (error) {
-            console.log(error);
-
-        }
-    }
-    checkDescription(info) {
-        /**
-         * Check if the description info is available in the response json from the API call.
-         * @param {json} info - Book information in JSON.
-         * @return {string} Book's description.
-         */
-        try {
-            if (info.description) {
-                return info.description;
-            }
-        } catch (error) {
-            console.log(error);
-
-        }
-    };
-    checkPageCount(info) {
-        /**
-         * Check if the page count info is available in the response json from the API call.
-         * @param {json} info - Book information in JSON.
-         * @return {string} Book's page count.
-         */
-        try {
-            if (info.pageCount) {
-                return info.pageCount;
-            }
-        } catch (error) {
-            console.log(error);
-
-        }
-    }
-    checkAverageRating(info) {
-        /**
-         * Check if the average rating info is available in the response json from the API call.
-         * @param {json} info - Book information in JSON.
-         * @return {string} Book's average rating.
-         */
-        try {
-            if (info.averageRating) {
-                return info.averageRating;
-            }
-        } catch (error) {
-            console.log(error);
-
-        }
-    }
-    checkCategories(info) {
-        /**
-         * Check if the categories info is available in the response json from the API call.
-         * @param {json} info - Book information in JSON.
-         * @return {string} Book's categories.
-         */
-        try {
-            var result = '';
-            if (info.categories) {
-                for (let i = 0; i < info.categories.length; i++) {
-                    const element = info.categories[i];
-                    result += element;
-                }
-                return result;
-            }
-        } catch (error) {
-            console.log(error);
-
-        }
-    }
-    checkPreviewLink(info) {
-        /**
-         * Check if the preview link info is available in the response json from the API call.
-         * @param {json} info - Book information in JSON.
-         * @return {string} Book's web reader link.
-         */
-        try {
-            if (info.webReaderLink) {
-                return info.webReaderLink;
-            }
-        } catch (error) {
-            console.log(error);
-
-        }
-    };
-    checkLanguage(info) {
-        /**
-         * Check if the language info is available in the response json from the API call.
-         * @param {json} info - Book information in JSON.
-         * @return {string} Book's language.
-         */
-        try {
-            if (info.language) {
-                return info.language;
-            }
-        } catch (error) {
-            console.log(error);
-
-        }
-    }
-    checkInfoLink(info) {
+    checkInfo(key, info) {
         /**
          * Check if the info link info is available in the response json from the API call.
          * @param {json} info - Book information in JSON.
          * @return {string} Book's information link.
          */
         try {
-            if (info.infoLink) {
-                return info.infoLink;
+            if (key === 'smallThumbnail') {
+                if (info.imageLinks.smallThumbnail) {
+                    return info.imageLinks.smallThumbnail;
+                }
+            }
+            else if (key === 'categories') {
+                var result = '';
+                if (info.categories) {
+                    for (let i = 0; i < info.categories.length; i++) {
+                        const element = info.categories[i];
+                        result += element;
+                    }
+                    return result;
+                }
+            }
+            else {
+                if (info[key]) {
+                    return info[key];
+                }
             }
         } catch (error) {
             console.log(error);
-
         }
     }
+
 
     render() {
         return (
@@ -209,27 +57,27 @@ class SearchResults extends Component {
                                         <Panel.Title toggle id={"panel-title" + book.id}>
                                             <Media id={"media-" + book.id}>
                                                 <Media.Left id={"media-left-" + book.id}>
-                                                    <img width={64} height={64} alt='' src={this.checkThumbnail(book.volumeInfo)} />
+                                                    <img width={64} height={64} alt='' src={this.checkInfo('smallThumbnail', book.volumeInfo)} />
                                                 </Media.Left>
                                                 <Media.Body id={"media-body-" + book.id}>
-                                                    <Media.Heading id={"media-heading-" + book.id}>{this.checkTitle(book.volumeInfo)}</Media.Heading>
+                                                    <Media.Heading id={"media-heading-" + book.id}>{this.checkInfo('title', book.volumeInfo)}</Media.Heading>
                                                     <p>
-                                                        {this.checkAuthors(book.volumeInfo)},
-                                                    <em>   {this.checkPublishers(book.volumeInfo)}</em>,
-                                                    <em>   {this.checkPublishedDate(book.volumeInfo)}</em>
+                                                        {this.checkInfo('authors', book.volumeInfo)},
+                                                    <em>   {this.checkInfo('publisher', book.volumeInfo)}</em>,
+                                                    <em>   {this.checkInfo('publishedDate', book.volumeInfo)}</em>
                                                     </p>
                                                 </Media.Body>
                                             </Media>
                                         </Panel.Title>
                                     </Panel.Heading>
                                     <Panel.Body collapsible id={"panel-body-" + book.id}>
-                                        <p><b>Description:</b> {this.checkDescription(book.volumeInfo)}</p>
-                                        <p><b>Average Rating:</b> {this.checkAverageRating(book.volumeInfo)}/5</p>
-                                        <p><b>Category:</b> {this.checkCategories(book.volumeInfo)}</p>
-                                        <p><b>Page number:</b> {this.checkPageCount(book.volumeInfo)} pages</p>
-                                        <p><b>Language:</b> {this.checkLanguage(book.volumeInfo)}</p>
-                                        <p><a href={this.checkPreviewLink(book.accessInfo)} target="_blank">Preview</a></p>
-                                        <p><a href={this.checkInfoLink(book.volumeInfo)} target="_blank">More..</a></p>
+                                        <p><b>Description:</b> {this.checkInfo('description', book.volumeInfo)}</p>
+                                        <p><b>Average Rating:</b> {this.checkInfo('averageRating', book.volumeInfo)}/5</p>
+                                        <p><b>Category:</b> {this.checkInfo('categories', book.volumeInfo)}</p>
+                                        <p><b>Page number:</b> {this.checkInfo('pageCount', book.volumeInfo)} pages</p>
+                                        <p><b>Language:</b> {this.checkInfo('language', book.volumeInfo)}</p>
+                                        <p><a href={this.checkInfo('webReaderLink', book.accessInfo)} target="_blank">Preview</a></p>
+                                        <p><a href={this.checkInfo('infoLink', book.volumeInfo)} target="_blank">More..</a></p>
                                     </Panel.Body>
                                 </Panel>
                             </PanelGroup>
